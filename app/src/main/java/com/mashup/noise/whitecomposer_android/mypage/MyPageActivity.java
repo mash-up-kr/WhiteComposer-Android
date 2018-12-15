7d4589d11cb3a6612c21d2920aecad8052d55b2a
snaag <04_miffy@naver.com>
2018-12-15 10:38:59 +0900

    [MyPage] MyPage의 activity(MyPageActivity), package(mypage) 추가함 - 기능 명세에 따른 디자인이 아직 나오지 않아, 액티비티만 만들어 놓았음 (구현 X)

---
+++ app/src/main/java/com/mashup/noise/whitecomposer_android/mypage/MyPageActivity.java
@@ -0,0 +1,16 @@
+package com.mashup.noise.whitecomposer_android.mypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mashup.noise.whitecomposer_android.R;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
