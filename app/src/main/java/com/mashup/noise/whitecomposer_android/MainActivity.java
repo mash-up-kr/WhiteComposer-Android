33948005092b053fbe3165cbf68a479d4ae86ec0
snaag <04_miffy@naver.com>
2019-01-05 16:03:54 +0900

    mypage fragment 추가

--- app/src/main/java/com/mashup/noise/whitecomposer_android/MainActivity.java
+++ app/src/main/java/com/mashup/noise/whitecomposer_android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mashup.noise.whitecomposer_android;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -8,13 +9,23 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.mashup.noise.whitecomposer_android.ViewInfo.MainView;
+import com.mashup.noise.whitecomposer_android.mypage.MyPageFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements MyPageFragment.OnFragmentInteractionListener {
 
     private static final String TAG = "MainActivity";
+    private ConstraintLayout mainLayout;
+
+    /* Fragment */
+    FragmentTransaction transaction;
+    private MyPageFragment myPageFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MainView mainView = new MainView(this);
         setSupportActionBar((BottomAppBar) findViewById(R.id.bottomBar));
+
+        initFragment();
     }
 
     @Override
@@ -44,8 +57,22 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_my:
                 Log.d(TAG, "onOptionsItemSelected: myPage");
                 Toast.makeText(this, "my page", Toast.LENGTH_SHORT).show();
+                transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, myPageFragment);
+
                 break;
         }
+        transaction.addToBackStack(null);
+        transaction.commit();
         return true;
+    }
+
+    public void initFragment() {
+        myPageFragment = new MyPageFragment();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
